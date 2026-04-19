966fba38262e5f9c4d9ec453c923585401936055
blessingmikay19-cyber <blessingmikay005@gmail.com>
2026-04-19 17:12:54 +0200

    added intro-loader with welcome message and linked universalScript.js in home.html

--- DPM Website/main/scripts/universalScript.js
+++ DPM Website/main/scripts/universalScript.js
@@ -1,1 +1,15 @@
+window.addEventListener("load", (_)=> {
+    let name = document.querySelector(".name");
+    let intro = document.querySelector(".intro-loader");
 
+    setTimeout(() => {
+        name.style.opacity = "1";
+        name.style.transform = "translateY(0)";
+    },300)
+
+    setTimeout(() => {
+        intro.style.top = "-100%";
+    },2000)
+})
+
+// This script above handles the introductory animation on the home page, displaying a welcome message and then sliding it up to reveal the main content. 
